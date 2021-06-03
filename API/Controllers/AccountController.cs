80a2ab28519409b76f716899a1b95cb6d8f6c9a0
Ryan Bertram <bertramdryan@gmail.com>
2021-06-03 11:50:49 -0400

    first commit

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+  public class AccountController : BaseApiController
+  {
+    private readonly DataContext _context;
+    public AccountController(DataContext context)
+    {
+      _context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<AppUser>> Register() {
+        
+    }
+  }
+}
